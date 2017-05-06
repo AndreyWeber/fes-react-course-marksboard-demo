7f64f789e8759955067944a10bb3d72f7651fdd3
Andrey Weber <Andrey.Weber@gmail.com>
2017-05-07 00:43:22 +0200

    Implemented support of switching underlying Google spreadsheets via URL parameter named 'ssname'

--- static/config.js
+++ static/config.js
@@ -1,7 +1,16 @@
+/* eslint-disable no-unused-vars */
 var APP_CONFIG = {
-    apiKey:         'AIzaSyDu8kDrHdk0QEFCYe0LZn4rm7phXZ9tJ7g',
-    clientId:       '326560008785-g3iungt3ffj1lk2dpv67001ati8rgmm8.apps.googleusercontent.com',
-    spreadsheetId:  '1EP4c-9UZVkEb4n4eM2fU5JYuBYZxUAuXXeppDzV0rjg',
-    discoveryUrl:   'https://sheets.googleapis.com/$discovery/rest?version=v4',
-    scope:          'https://www.googleapis.com/auth/spreadsheets.readonly'
+    commonCredentials: {
+        apiKey:         'AIzaSyDu8kDrHdk0QEFCYe0LZn4rm7phXZ9tJ7g',
+        clientId:       '326560008785-g3iungt3ffj1lk2dpv67001ati8rgmm8.apps.googleusercontent.com',
+        discoveryUrl:   'https://sheets.googleapis.com/$discovery/rest?version=v4',
+        scope:          'https://www.googleapis.com/auth/spreadsheets.readonly'
+    },
+    availableSpreadsheets: [{
+        id:     '1EP4c-9UZVkEb4n4eM2fU5JYuBYZxUAuXXeppDzV0rjg',
+        name:   'REACT2 HW'
+    }, {
+        id:     '1C7PWqTPcK1IbDWTbYH_NkDQOszKD5dtVW51i9SW-UxU',
+        name:   'EXAMPLE / HW'
+    }]
 };
